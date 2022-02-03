5207ac08e3b64bc77604625cbbe61e9508561a95
Dobriy26 <98391572+Dobriy26@users.noreply.github.com>
2022-02-03 16:56:30 +0300

    Add calculator

---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Dynamic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour
+{
+    public Text text1;
+    public string calc1;
+    public string x1;
+
+    public void On_Click_Button()
+    {
+        text1.text += calc1;
+    }
+    
+    public void On_Click_C()
+    {
+        text1.text = "";
+    }
+    
+    public void On_Click_Equal()
+    {
+        DataTable dt = new DataTable();
+        x1 = (dt.Compute(text1.text, "")).ToString();
+        text1.text = x1;
+    }
+}
