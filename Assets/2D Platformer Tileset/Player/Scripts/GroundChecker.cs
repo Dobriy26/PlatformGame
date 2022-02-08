9f04fd42a5cf17786ac0cd92d9d6d7f7a11e5eee
Dobriy26 <98391572+Dobriy26@users.noreply.github.com>
2022-02-08 22:14:28 +0300

    Animator+UI

---
+++ Assets/2D Platformer Tileset/Player/Scripts/GroundChecker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+  [SerializeField]
+  private float _radius;
+  [SerializeField]
+  private LayerMask _layerMask;
+
+  public bool IsGrounded()
+  {
+    return Physics2D.OverlapCircle(transform.position, _radius,_layerMask);
+  }
+}
--- Assets/2D Platformer Tileset/Player/Scripts/PlayerController.cs
+++ Assets/2D Platformer Tileset/Player/Scripts/PlayerController.cs
@@ -1,26 +1,52 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Core.Scripts.Input;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float Speed = 14f;
     public float Assel = 6f;
-    public float Jump = 5f;
+    public GroundChecker groundChecker;
+    public float jumpForce;
     
     private Vector2 _input;
     private Rigidbody2D _rb;
+    private Animator _animator;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        groundChecker = GetComponentInChildren<GroundChecker>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        _input.x = Input.GetAxis("Horizontal");
-        _input.y = Input.GetAxis("Vertical");
+        var isGround = groundChecker.IsGrounded();
+        _input.x = CrossPlatformInputManager.GetAxis("Horizontal");
+        if(CrossPlatformInputManager.GetButtonDown("Jump") && _rb.velocity.y == 0 && isGround)
+            _rb.AddForce(Vector2.up * jumpForce);
+        if(Math.Abs(_input.x) > 0 && _rb.velocity.y == 0 && isGround)
+            _animator.SetBool("IsRunning", true);
+        else
+            _animator.SetBool("IsRunning", false);
+
+        if (_rb.velocity.y == 0 && isGround)
+        {
+            _animator.SetBool("IsJumping", false);
+            _animator.SetBool("IsFallen", false);
+        }
+            
+        if(_rb.velocity.y > 0)
+            _animator.SetBool("IsJumping", true);
+
+        if (_rb.velocity.y < 0)
+        {
+            _animator.SetBool("IsJumping", false);
+            _animator.SetBool("IsFallen", true);
+        }
     }
 
     private void FixedUpdate()
@@ -29,10 +55,5 @@ public class PlayerController : MonoBehaviour
         var _xVelosity = _input.x == 0 ? 0 : _rb.velocity.x;
         _rb.AddForce(new Vector2((_input.x * Speed-_rb.velocity.x) * a, 0));
         _rb.velocity = new Vector2(_xVelosity, _rb.velocity.y);
-        
-        var b = Assel;
-        var _yVelosity = _input.y == 0 ? 0 : _rb.velocity.y;
-        _rb.AddForce(new Vector2(0, (_input.y * Jump-_rb.velocity.y) * a));
-        _rb.velocity = new Vector2(_rb.velocity.x,_yVelosity);
     }
 }
