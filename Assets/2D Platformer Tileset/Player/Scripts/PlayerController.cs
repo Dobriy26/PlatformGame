54bf2b5e1af4b3342962415cb93ddffb879621cd
Dobriy26 <98391572+Dobriy26@users.noreply.github.com>
2022-02-08 13:31:03 +0300

    Animations of player

---
+++ Assets/2D Platformer Tileset/Player/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 14f;
+    public float Assel = 6f;
+
+    private Vector2 _input;
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _input.x = Input.GetAxis("Horizontal");
+        _input.y = Input.GetAxis("Jump");
+    }
+
+    private void FixedUpdate()
+    {
+        var a = Assel;
+        var _xVelosity = _input.x == 0 ? 0 : _rb.velocity.x;
+        _rb.AddForce(new Vector2((_input.x * Speed-_rb.velocity.x) * a, 0));
+        _rb.velocity = new Vector2(_xVelosity, _rb.velocity.y);
+    }
+}
---
+++ Assets/Camera/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Camera
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform CameraTarget;
+        public float CameraSpeed = 2.5f;
+
+        private void FixedUpdate()
+        {
+            var _newPosition = Vector2.Lerp(transform.position, 
+                CameraTarget.position, Time.deltaTime * CameraSpeed);
+            var CameraPosition = new Vector3(_newPosition.x, _newPosition.y, -10);
+            transform.position = CameraPosition;
+        }
+        
+    }
+}
