a8617a21109314895f6367a5482ebc2aff947357
Dobriy26 <98391572+Dobriy26@users.noreply.github.com>
2022-03-10 21:20:19 +0300

    camera follow

--- Assets/Scripts/CameraFollowing.cs
+++ Assets/Scripts/CameraFollowing.cs
@@ -6,16 +6,16 @@ public class CameraFollowing : MonoBehaviour
 { 
     [SerializeField]
     private Transform target;
-   [SerializeField]
-   private 
+
+    [SerializeField] private Vector3 _offset;
     void Start()
     {
-        
+        _offset = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = target.position + _offset;
     }
 }
