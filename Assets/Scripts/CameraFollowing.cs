3684a3460613a6e7787a656afa3a79d88ebd2bb2
Dobriy26 <98391572+Dobriy26@users.noreply.github.com>
2022-03-10 21:17:18 +0300

    camera follow

---
+++ Assets/Scripts/CameraFollowing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour
+{ 
+    [SerializeField]
+    private Transform target;
+   [SerializeField]
+   private 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
