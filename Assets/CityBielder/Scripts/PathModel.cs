55705f42dfa518eea6956444bce7ddc50ef9584a
Dobriy26 <98391572+Dobriy26@users.noreply.github.com>
2022-04-07 22:03:50 +0300

    CityBielder

---
+++ Assets/CityBielder/Scripts/PathModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathModel : MonoBehaviour
+{
+    [SerializeField] private GameObject[] points;
+    public List<(Vector3, Vector3)> Ways;
+
+    private void Awake()
+    {
+        Ways = new List<(Vector3, Vector3)>();
+        for (int i = 0; i < points.Length; i++)
+        {
+            Ways.Add((points[i].transform.position, points[i+1].transform.position));
+        }
+    }
+}
---
+++ Assets/CityBielder/Scripts/TouchController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
+
+public class TouchController : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    
+    private Vector3 _touchStart;
+    private const int leftCameraLimit = -50;
+    private const int rightCameraLimit = 50;
+    private Vector3 GetWorldPosition()
+    {
+        var mousePos = mainCamera.ScreenPointToRay(Input.mousePosition);
+        var ground = new Plane(Vector3.forward,Vector3.zero);
+        ground.Raycast(mousePos, out var dist);
+        return mousePos.GetPoint(dist);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _touchStart = GetWorldPosition();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            var direction = _touchStart - GetWorldPosition();
+            mainCamera.transform.position += new Vector3(direction.x, 0, 0);
+            
+        }
+
+        var x = mainCamera.transform.position.x;
+        x = Math.Min(x, rightCameraLimit);
+        x = Math.Max(x, leftCameraLimit);
+        mainCamera.transform.position = new Vector3(x, mainCamera.transform.position.y, mainCamera.transform.position.z);
+    }
+}
